4b732365425f3a6a363e857514234768e6b5c3d3
Daniel Hunninghake <daniel@drip.fm>
2016-02-18 15:04:53 -0500

    z-index bug

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,8 +7,7 @@ class Nav extends React.Component {
   render() {
     return (
       <header className='pt2 pb1 h4 montserrat-reg'>
-        
-        <a className='bg-white gray-dark contact circle p1' href='mailto:d@dhunninghake.com'>
+        <a className='contact bg-white gray-dark circle p1 z1' href='mailto:d@dhunninghake.com'>
           <Icon classes={'block'} icon={'icon-contact'} width={18} height={18} viewbox={'0 0 512 512'} />
         </a>
       </header>
