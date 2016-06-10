7b10f1816897c74cacfa047a9220125bc2f73ed8
Daniel Hunninghake <daniel@drip.fm>
2016-06-10 15:40:27 -0400

    basic copy

--- src/app/handlers/App.js
+++ src/app/handlers/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default props => {
   return (
     <div>
-      <h1>Application</h1>
       <div>{props.children}</div>
     </div>
   )
 }
+
--- src/app/handlers/Home.js
+++ src/app/handlers/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default props => {
   return (
-    <h1>I am Home</h1>   
+    <div>
+      <h1>Daniel Hunninghake</h1>
+      <h4>I’m a designer/developer hustling to bring ideas to life.<br />Currently making artful interfaces at <a href='http://sanctuary.computer/'>Sanctuary Computer</a>.</h4>
+    </div>
   );
 }
