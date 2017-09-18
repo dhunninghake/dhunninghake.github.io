562430f00c33f720cb6e683cd50c9f1535b22b9a
Daniel Hunninghake <dhunn1587@gmail.com>
2017-09-18 10:31:05 -0400

    Cleanup

--- src/app.js
+++ src/app.js
@@ -7,23 +7,115 @@ require('./fonts.css');
 
 console.log(v.composes);
 
-const App = () => {
-  const styles = v({
-    container: {
-      fontFamily: '"space_monoregular", Helvetica',
-      padding: '4rem'
-    },
-    headline: {
-      fontSize: '3vw',
-      width: '60vw'
-    }
+const Row = ({ children, height }) => (
+  <div style={{ display: 'table-row', height }}>
+    {children}
+  </div>
+);
 
-  });
+const Col = ({ children, align }) => (
+  <div style={{ display: 'table-cell', verticalAlign: align }}>
+    {children}
+  </div>
+);
+
+const styles = v({
+  container: {
+    fontFamily: '"space_monoregular", Helvetica',
+    boxSizing: 'border-box',
+    padding: '4rem',
+    display: 'table',
+    height: '100%',
+    position: 'absolute',
+    width: '100%',
+    top: 0,
+    left: 0,
+    color: '#333'
+  },
+  list: {
+    listStyleType: 'none',
+    padding: 0,
+    'li': {
+      display: 'inline-block',
+      marginRight: '2rem',
+      'span': {
+        fontSize: '.75rem',
+        display: 'inline-block',
+        marginRight: '.25rem'
+      },
+      'a': {
+        color: 'inherit'
+      }
+    }
+  },
+  header: {
+
+  },
+  headline: {
+    fontSize: '2.8vw',
+    width: '56vw',
+    marginBottom: '1rem'
+  },
+  name: {
+    fontWeight: 'normal'
+  }
+});
+
+const links = [
+  {
+    symbol: String.fromCharCode(9824),
+    link: 'https://www.instagram.com/dhunninghake/',
+    name: 'Instagram'
+  },
+  {
+    symbol: String.fromCharCode(9827),
+    link: 'https://twitter.com/dhunninghake',
+    name: 'Twitter'
+  },
+  {
+    symbol: String.fromCharCode(9829),
+    link: 'https://github.com/dhunninghake',
+    name: 'Github'
+  },
+  {
+    symbol: String.fromCharCode(9830),
+    link: 'mailto:d@dhunninghake.com',
+    name: 'Email'
+  }
+];
+
+const App = () => {
   return (
     <div className={styles.container}>
-      <Icon name='cool' width={46} />
-      <h1>{'Daniel Hunninghake'}</h1>
-      <p className={styles.headline}>{'I’m a product designer & developer focused on empowering creators through amazing tech. Based in Brooklyn,NY.'}</p>
+      <Row height={'20%'}>
+        <Col align={'top'}>
+          <header>
+            <h4 className={styles.name}>{'Daniel'}<br/ >{'Hunninghake'}</h4>
+            <Icon name='cool' width={46} />
+
+          </header>
+        </Col>
+      </Row>
+      <Row height={'60%'}>
+        <Col align={'middle'}>
+          <p className={styles.headline}>{'I’m a product designer & developer focused on empowering creators through amazing tech. Based in Brooklyn,NY.'}</p>
+          <ul className={styles.list}>
+            {links.map((link) => (
+              <li key={link.name}>
+                <span>{link.symbol}</span>
+                <a href={link.link}>{link.name}</a>
+              </li>
+            ))}
+          </ul>
+        </Col>
+      </Row>
+      <Row height={'20%'}>
+        <Col align={'bottom'}>
+          <footer>
+            {'footer'}
+          </footer>
+        </Col>
+      </Row>
     </div>
   );
 };
@@ -32,11 +124,3 @@ render(
   <App />,
   document.getElementById('app')
 );
-
-
-// <ul className={styles.list}>
-//   <li><a href='https://www.instagram.com/dhunninghake/' target='_blank'>Instagram</a></li>
-//   <li><a href='https://twitter.com/dhunninghake' target='_blank'>Twitter</a></li>
-//   <li><a href='https://github.com/dhunninghake' target='_blank'>Github</a></li>
-//   <li><a href='mailto:d@dhunninghake.com'>Email</a></li>
-// </ul>
